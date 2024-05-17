37baec80ab90776e7acb90d26aac401606f95e13
Vincent Barbier <vincentpcrog@hotmail.com>
2024-05-17 13:56:33 +0200

    Add .gitignore and print cost

--- precision.py
+++ precision.py
@@ -2,16 +2,19 @@ import csv
 import matplotlib.pyplot as plt
 
 def main():
-	with open('cost.csv', 'r') as file:
-		data = csv.reader(file)
-		cost = []
-		for row in data:
-			cost.append(float(row[0]))
-		plt.plot(cost)
-		plt.ylabel('Cost')
-		plt.xlabel('Iteration')
-		plt.savefig('cost.png')
-
+	try:
+		with open('cost.csv', 'r') as file:
+			data = csv.reader(file)
+			cost = []
+			for row in data:
+				cost.append(float(row[0]))
+			print("Cost:", cost[-1])
+			plt.plot(cost)
+			plt.ylabel('Cost')
+			plt.xlabel('Iteration')
+			plt.savefig('cost.png')
+	except:
+		print("Error: cost.csv not found")
 
 if __name__ == "__main__":	
 	main()
